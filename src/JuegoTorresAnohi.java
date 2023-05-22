da3f56539a7f5255f4627fac53152f0125e6a5b8
Juniorelmusico <juniorationlj@gmail.com>
2023-05-21 23:39:27 -0500

    Initial commit 5.0

---
+++ src/JuegoTorresAnohi.java
@@ -0,0 +1,34 @@
+public class JuegoTorresAnohi {
+    private int numDiscos;
+    private Torre torreOrigen;
+    private Torre torreDestino;
+    private Torre torreAuxiliar;
+
+    public JuegoTorresAnohi(int numDiscos) {
+        this.numDiscos = numDiscos;
+        torreOrigen = new Torre();
+        torreDestino = new Torre();
+        torreAuxiliar = new Torre();
+    }
+
+    public void resolver() {
+        moverDiscos(numDiscos, torreOrigen, torreDestino, torreAuxiliar);
+    }
+
+    private void moverDiscos(int n, Torre origen, Torre destino, Torre auxiliar) {
+        if (n == 1) {
+            int disco = origen.pop();
+            destino.push(disco);
+            System.out.println("Mover disco " + disco + " de " + origen + " T1 " + destino);
+            return;
+        }
+
+        moverDiscos(n - 1, origen, auxiliar, destino);
+
+        int disco = origen.pop();
+        destino.push(disco);
+        System.out.println("Mover disco " + disco + " de " + origen + " T1 " + destino);
+
+        moverDiscos(n - 1, auxiliar, destino, origen);
+    }
+}
---
+++ src/Torre.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+public class Torre {
+    private Stack<Integer> discos;
+
+    public Torre() {
+        discos = new Stack<>();
+    }
+
+    public void push(int disco) {
+        discos.push(disco);
+    }
+
+    public int pop() {
+        return discos.pop();
+    }
+
+    public int peek() {
+        return discos.peek();
+    }
+
+    public boolean estaVacia() {
+        return discos.isEmpty();
+    }
+}
+
